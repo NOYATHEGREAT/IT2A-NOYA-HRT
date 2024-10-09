a8f963e95d155ff64e5b2a4e707a80c613cd8522
NOYATHEGREAT <johnlloydisguapo@gmail.com>
2024-10-09 11:51:07 +0800

    update system

--- src/it2a/noya/htr/IT2ANOYAHTR.java
+++ src/it2a/noya/htr/IT2ANOYAHTR.java
@@ -1,39 +1,68 @@
-
 package it2a.noya.htr;
 
 import java.util.Scanner;
+
 public class IT2ANOYAHTR {
-    
-    public void addPatients(){
-        Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
         config conf = new config();
+        Scanner sc = new Scanner(System.in);
+        String response;
+
+        do {
+            System.out.println("1. ADD PATIENT");
+            System.out.println("2. VIEW PATIENT");
+            System.out.println("3. UPDATE PATIENT");
+            System.out.println("4. DELETE PATIENT");
+            System.out.println("5. EXIT");
+
+            System.out.print("Enter Action: ");
+            int action = sc.nextInt();
+
+            IT2ANOYAHTR sample = new IT2ANOYAHTR();
+            switch (action) {
+                case 1:
+                    sample.addPatients(conf);  
+                    break;
+                case 2:
+                    conf.viewRecords();
+                    break;
+                case 3:
+                    
+                    break;
+                case 4:
+                    
+                    break;
+                case 5:
+                    System.out.println("Exiting...");
+                    break;
+                default:
+                    System.out.println("Invalid action. Please try again.");
+            }
+            System.out.print("Do you want to continue? (Y/N): ");
+            response = sc.next();
+
+        } while (response.equalsIgnoreCase("y"));
+
+        sc.close();
+    }
+
+    public void addPatients(config conf) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Patient ID: ");
+        String id = sc.next();
         System.out.print("Patient First Name: ");
         String fname = sc.next();
         System.out.print("Patient Last Name: ");
         String lname = sc.next();
-        System.out.print("Patient Date of Birth:  ");
-        String dateOfbirth = sc.next();
+        System.out.print("Patient Email: ");
+        String email = sc.next();
         System.out.print("Patient Status: ");
         String status = sc.next();
-        System.out.print("patient Contact: ");
-        String contact = sc.next();
-        System.out.print("patient Treatment/Diagnosis: ");
-        String TreatDiag = sc.next();
-        System.out.print("patient AdmissionDate: ");
-        String Admission = sc.next();
-        System.out.print("patient Discharge: ");
-        String Discharge = sc.next();
-        
-        String sql = "INSERT INTO Student (s_fname, s_lname, s_email, s_status) VALUES (?, ?, ?, ?)";
 
-
-        conf.addRecord(sql, fname, lname, status);
-
-
+       
+        String sql =("INSERT INTO Patients (p_id, p_fname, p_lname, p_email, p_status) VALUES (?, ?, ?, ?, ?)");
+        conf.addRecord(sql, id, fname, lname, email, status);
     }
-    public static void main(String[] args) {
-        
-        
-    }
-    
 }
+
--- src/it2a/noya/htr/config.java
+++ src/it2a/noya/htr/config.java
@@ -4,39 +4,97 @@ package it2a.noya.htr;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class config {
-public static Connection connectDB() {
+
+    public static Connection connectDB() {
         Connection con = null;
         try {
-            Class.forName("org.sqlite.JDBC"); 
-            con = DriverManager.getConnection("jdbc:sqlite:HTR-NOYA.db"); 
+            Class.forName("org.sqlite.JDBC");
+            con = DriverManager.getConnection("jdbc:sqlite:HTR-NOYA.db");
             System.out.println("Connection Successful");
         } catch (Exception e) {
             System.out.println("Connection Failed: " + e);
         }
         return con;
-}
-
-    public void addRecord(String sql, String... values) {
-        try (Connection conn = this.connectDB(); 
-             PreparedStatement pstmt = conn.prepareStatement(sql)) {
-
-          
-            for (int i = 0; i < values.length; i++) {
-                pstmt.setString(i + 1, values[i]); 
-            }
-
-            pstmt.executeUpdate();
-            System.out.println("Record added successfully!");
-        } catch (SQLException e) {
-            System.out.println("Error adding record: " + e.getMessage());
-        }
     }
 
+   public void addRecord(String sql, Object... values) {
+    try (Connection conn = this.connectDB();
+         PreparedStatement pstmt = conn.prepareStatement(sql)) {
 
+        
+        for (int i = 0; i < values.length; i++) {
+            if (values[i] instanceof Integer) {
+                pstmt.setInt(i + 1, (Integer) values[i]); 
+            } else if (values[i] instanceof Double) {
+                pstmt.setDouble(i + 1, (Double) values[i]); 
+            } else if (values[i] instanceof Float) {
+            } else if (values[i] instanceof Long) {
+                pstmt.setLong(i + 1, (Long) values[i]); 
+            } else if (values[i] instanceof Boolean) {
+                pstmt.setBoolean(i + 1, (Boolean) values[i]); 
+            } else if (values[i] instanceof java.util.Date) {
+                pstmt.setDate(i + 1, new java.sql.Date(((java.util.Date) values[i]).getTime())); 
+            } else if (values[i] instanceof java.sql.Date) {
+                pstmt.setDate(i + 1, (java.sql.Date) values[i]);
+            } else if (values[i] instanceof java.sql.Timestamp) {
+                pstmt.setTimestamp(i + 1, (java.sql.Timestamp) values[i]); 
+            } else {
+                pstmt.setString(i + 1, values[i].toString()); 
+            }
+        }
 
-
+        pstmt.executeUpdate();
+        System.out.println("Record added successfully!");
+    } catch (SQLException e) {
+        System.out.println("Error adding record: " + e.getMessage());
+    }
 }
+    public void addPatients() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Patient ID: ");
+        String id = sc.next();
+        System.out.print("Patient First Name: ");
+        String fname = sc.next();
+        System.out.print("Patient Last Name: ");
+        String lname = sc.next();
+        System.out.print("Patient Email: ");
+        String email = sc.next();
+        System.out.print("Patient Status: ");
+        String status = sc.next();
 
+    
+        String sql = "INSERT INTO Patients (p_id, p_fname, p_lname, p_email, p_status) VALUES (?, ?, ?, ?, ?)";
+        addRecord(sql, id, fname, lname, email, status);
+    }
+
+    public void viewRecords() {
+        String sql = "SELECT * FROM Patients"; 
+        try (Connection conn = connectDB();
+             PreparedStatement pstmt = conn.prepareStatement(sql);
+             ResultSet rs = pstmt.executeQuery()) {
+
+            System.out.println("--------------------------------------------------------------------------------");
+            System.out.printf("| %-5s | %-20s | %-12s | %-25s | %-10s |\n", "Patient ID", "Firstname", "LastName", "Email", "Status");
+            System.out.println("--------------------------------------------------------------------------------");
+
+            while (rs.next()) {
+                int id = rs.getInt("p_id");
+                String fname = rs.getString("p_fname");
+                String lname = rs.getString("p_lname");
+                String email = rs.getString("p_email");
+                String sts = rs.getString("p_status");
+
+                System.out.printf("| %-5d | %-20s | %-12s | %-25s | %-10s |\n", id, fname, lname, email, sts);
+            }
+
+            System.out.println("--------------------------------------------------------------------------------");
+        } catch (SQLException e) {
+            System.out.println("Error retrieving records: " + e.getMessage());
+        }
+    }
+}
